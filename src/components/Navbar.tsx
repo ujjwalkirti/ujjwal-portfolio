472e80e79a5205bd390367af9b47e4c867fd2687
ujjwalkirti <kirti.chargers@gmail.com>
2023-02-19 02:03:08 +0530

    initial commit

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Navbar = () => {
+  return <section className="h-[54px]"></section>;
+};
+
+export default Navbar;
---
+++ src/components/landing-page/LandingPage.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+const LandingPage = () => {
+  return (
+    <section className="bg-[#0a192f] min-h-screen">
+      <Navbar />
+    </section>
+  );
+};
+
+export default LandingPage;
