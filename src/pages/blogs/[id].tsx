1ebd08aeb973e570260069be692ef36db28a9949
Ujjwal Kirti <kirti.chargers@gmail.com>
2023-07-06 15:16:53 +0530

    Bug fix: making sure that no error is thrown when trying to access page that doesnot exist

--- src/pages/blogs/[id].tsx
+++ src/pages/blogs/[id].tsx
@@ -14,34 +14,16 @@ import { Skeleton, SkeletonCircle, SkeletonText } from "@chakra-ui/react";
 import { Box } from "@chakra-ui/react";
 import CommentsSection from "@/components/blogs/CommentsSection";
 import VotesDisplayer from "@/components/blogs/VotesDisplayer";
+import { NextPageContext } from "next";
 
 type props = {
-  id: string;
+  blog: Blog;
   server_comments: Blog_Comment[];
 };
 
-const ReadBlog = ({ id, server_comments }: props) => {
+const ReadBlog = ({ blog, server_comments }: props) => {
   const [comments, setComments] = useState(server_comments);
-  const [isLoaded, setIsLoaded] = React.useState(false);
-  const [blog, setBlog] = useState<any>({});
   const titleRef = useRef(null);
-
-  useEffect(() => {
-    async function fetchBlogFromId(id: string) {
-      const { data, error } = await supabase
-        .from("posts")
-        .select("*")
-        .eq("id", id);
-
-      if (error) {
-        setBlog(null);
-      }
-      setBlog(data[0]);
-    }
-    fetchBlogFromId(id).then(() => {
-      setIsLoaded(true);
-    });
-  }, []);
 
   useEffect(() => {
     const channel = supabase
@@ -63,15 +45,33 @@ const ReadBlog = ({ id, server_comments }: props) => {
     };
   }, [supabase]);
 
-  const parsedDate = moment(blog.created_at, "YYYY-MM-DDTHH:mm:ssZ");
-
   const handleScrollToTitle = () => {
     titleRef.current.scrollIntoView({ behavior: "smooth" });
   };
+  if (!blog)
+    return (
+      <div className={"bg-[#0a192f] min-h-screen " + monsterrat.className}>
+        <Head>
+          <title>Blogs</title>
+          <meta
+            name="description"
+            content="Blog written by Ujjwal Kirti, where he shares his experiences during his journey as Web Developer"
+          />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link rel="icon" href="/UK.png" />
+        </Head>
+        <Box padding="6" boxShadow="lg" className="w-full">
+          <Skeleton m={"15"} height={"100px"} />
+          <SkeletonCircle size="10" />
+          <SkeletonText mt="4" noOfLines={10} spacing="4" skeletonHeight="4" />
+        </Box>
+      </div>
+    );
+
   return (
     <div className={"bg-[#0a192f] min-h-screen " + monsterrat.className}>
       <Head>
-        <title>{blog.title ? blog.title : "..."}</title>
+        <title>{blog.title}</title>
         <meta name="description" content={blog.intro} />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/UK.png" />
@@ -81,65 +81,57 @@ const ReadBlog = ({ id, server_comments }: props) => {
           {" "}
           <IoIosArrowBack className="bg-white text-2xl rounded-full hover:bg-[#64ffda] cursor-pointer text-[#0a192f] ml-3 mb-6" />
         </Link>
-        {isLoaded ? (
-          <div className="px-2  flex flex-col items-center gap-3">
-            {" "}
-            <div className="w-full flex flex-col items-center  dark-grayish-text text-md">
-              <p className="">{parsedDate.format("MMM DD, YYYY")}</p>
-            </div>
-            <p
-              ref={titleRef}
-              className={
-                "aqua text-3xl lg:text-4xl text-center font-semibold my-4 " +
-                libre_caslon_text.className
-              }
-            >
-              {blog.title}
+
+        <div className="px-2  flex flex-col items-center gap-3">
+          {" "}
+          <div className="w-full flex flex-col items-center  dark-grayish-text text-md">
+            <p className="">
+              {moment(blog.created_at, "YYYY-MM-DDTHH:mm:ssZ").format(
+                "MMM DD, YYYY"
+              )}
             </p>
-            <div className="relative h-56 lg:h-72 w-full lg:w-4/5 lg:mx-auto my-5">
-              <Image
-                src={blog.img_url}
-                alt="the blog's image"
-                fill
-                priority
-                className="object-cover"
-              />
-            </div>
-            <p
-              dangerouslySetInnerHTML={{ __html: blog.content }}
-              className="text-justify px-2 text-sm w-full md:w-11/12 lg:w-4/5 grayish-text"
-            ></p>
-            {/* upvotes and downvotes */}
-            {/* <VotesDisplayer upvotes={blog.upvotes} downvotes={blog.downvotes} /> */}
-            {/* Comments section */}
-            <CommentsSection comments={comments} blog_id={id} />
-            <button
-              onClick={() => {
-                handleScrollToTitle();
-              }}
-              className="bg-white text-blue-700 hover:bg-blue-700 hover:text-white text-xl font-semibold w-2/3 lg:w-1/4 text-center py-2 rounded-md flex items-center justify-center gap-3"
-            >
-              Go back to top! <RxPinTop />
-            </button>
-            <div className="text-3xl flex">
-              {" "}
-              <GoPrimitiveDot />
-              <GoPrimitiveDot />
-              <GoPrimitiveDot />
-            </div>
           </div>
-        ) : (
-          <Box padding="6" boxShadow="lg" className="w-full">
-            <Skeleton m={"15"} height={"100px"} />
-            <SkeletonCircle size="10" />
-            <SkeletonText
-              mt="4"
-              noOfLines={10}
-              spacing="4"
-              skeletonHeight="2"
+          <p
+            ref={titleRef}
+            className={
+              "aqua text-3xl lg:text-4xl text-center font-semibold my-4 " +
+              libre_caslon_text.className
+            }
+          >
+            {blog.title}
+          </p>
+          <div className="relative h-56 lg:h-72 w-full lg:w-4/5 lg:mx-auto my-5">
+            <Image
+              src={blog.img_url}
+              alt="the blog's image"
+              fill
+              priority
+              className="object-cover"
             />
-          </Box>
-        )}
+          </div>
+          <p
+            dangerouslySetInnerHTML={{ __html: blog.content }}
+            className="text-justify px-2 text-sm w-full md:w-11/12 lg:w-4/5 grayish-text"
+          ></p>
+          {/* upvotes and downvotes */}
+          {/* <VotesDisplayer upvotes={blog.upvotes} downvotes={blog.downvotes} /> */}
+          {/* Comments section */}
+          <CommentsSection comments={comments} blog_id={blog.id.toString()} />
+          <button
+            onClick={() => {
+              handleScrollToTitle();
+            }}
+            className="bg-white text-blue-700 hover:bg-blue-700 hover:text-white text-xl font-semibold w-2/3 lg:w-1/4 text-center py-2 rounded-md flex items-center justify-center gap-3"
+          >
+            Go back to top! <RxPinTop />
+          </button>
+          <div className="text-3xl flex">
+            {" "}
+            <GoPrimitiveDot />
+            <GoPrimitiveDot />
+            <GoPrimitiveDot />
+          </div>
+        </div>
       </div>
       <ToastContainer />
     </div>
@@ -148,9 +140,24 @@ const ReadBlog = ({ id, server_comments }: props) => {
 
 export default ReadBlog;
 
-export async function getServerSideProps(context: any) {
+async function fetchBlogFromId(id: string) {
+  const { data: posts, error } = await supabase
+    .from("posts")
+    .select("*")
+    .eq("id", id);
+
+  if (error) {
+    return null;
+  }
+  return posts;
+}
+
+export async function getServerSideProps(context: NextPageContext) {
   const table_id = context.query.id;
 
+  let blogs = await fetchBlogFromId(table_id as string);
+
+  console.log(blogs);
   // Fetch data from Supabase
   const { data, error } = await supabase
     .from("comments")
@@ -162,7 +169,7 @@ export async function getServerSideProps(context: any) {
     console.error("Error fetching data:", error);
     return {
       props: {
-        id: table_id,
+        blog: blogs.length === 0 ? null : blogs[0],
         server_comments: null,
       },
     };
@@ -170,7 +177,7 @@ export async function getServerSideProps(context: any) {
 
   return {
     props: {
-      id: table_id,
+      blog: blogs.length === 0 ? null : blogs[0],
       server_comments: data,
     },
   };
