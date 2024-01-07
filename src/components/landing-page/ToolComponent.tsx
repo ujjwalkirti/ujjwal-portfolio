0292d353eb58fdafa28431daa5d565e8b259cc81
Ujjwal Kirti <ujjwal.k@dotproduct.in>
2024-01-07 14:11:10 +0530

    feat: added some interactivity to ToolComponent.tsx

--- src/components/landing-page/ToolComponent.tsx
+++ src/components/landing-page/ToolComponent.tsx
@@ -4,7 +4,7 @@ function ToolComponent({ index, tool }) {
   return (
     <li
       key={index}
-      className="py-[20px] font-medium flex flex-col text-[15px] items-center gap-2 shadow-lg border m-3 rounded-md"
+      className="py-[10px] font-medium flex flex-col text-[15px] items-center gap-2 m-2 bg-slate-800/50 shadow-[inset_0_1px_0_0_rgba(148,163,184,0.1)] drop-shadow-lg rounded-md  transform active:scale-90 transition-transform duration-200 cursor-pointer"
     >
       <tool.icon className="aqua text-[40px]" />
       <p>{tool.title}</p>
